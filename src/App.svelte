4a9dac250d7c789dee72331ed2f6572b1a7bcc8c
Yugandhar182 <yugandhar@recruitly.io>
2023-06-08 15:53:23 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,77 @@
 <script>
-	export let name;
-</script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
+	import { onMount } from 'svelte';
+  
+	let gridContainer;
+	let agGrid;
+  
+	const columnDefs = [
+	  { field: 'make' },
+	  { field: 'model' },
+	  { field: 'price' },
+	  {
+		headerName: 'Actions',
+		cellRenderer: function(params) {
+		  return `<button onclick="editRow(${params.rowIndex})">Edit</button>`;
 		}
+	  }
+	];
+  
+	const rowData = [
+	  
+	{ make: 'suziki', model: 'access', price: 65000 },
+	{ make: 'Hero', model: 's1', price: 95000 },
+	{ make: 'Honda', model: 'Activa', price: 85000 },
+	{ make: 'Toyota', model: 'Celica', price: 35000 },
+	{ make: 'Ford', model: 'Mondeo', price: 32000 },
+	{ make: 'Porsche', model: 'Boxter', price: 72000 }
+	];
+  
+	function editRow(rowIndex) {
+	  const selectedRowData = agGrid.gridOptions.api.getDisplayedRowAtIndex(rowIndex).data;
+	  // Display the popup with the selected row data
+	  window.alert(JSON.stringify(selectedRowData, null, 2));
 	}
-</style>
+  
+	onMount(() => {
+	  import('ag-grid-community').then((module) => {
+		agGrid = module;
+		const gridOptions = {
+		  columnDefs: columnDefs,
+		  rowData: rowData,
+		  defaultColDef: {
+			sortable: true,
+			unSortIcon: true,
+			resizable: true,
+			flex: 1,
+			minWidth: 100,
+		  },
+		};
+		new agGrid.Grid(gridContainer, gridOptions);
+	  });
+	});
+  </script>
+  
+  <svelte:head>
+	<script src="https://cdn.jsdelivr.net/npm/ag-grid-community@latest/dist/ag-grid-community.min.noStyle.js"></script>
+	<link
+	  rel="stylesheet"
+	  href="https://cdn.jsdelivr.net/npm/ag-grid-community@latest/dist/styles/ag-grid.css"
+	/>
+	<link
+	  rel="stylesheet"
+	  href="https://cdn.jsdelivr.net/npm/ag-grid-community@latest/dist/styles/ag-theme-alpine.css"
+	/>
+  </svelte:head>
+  
+  <div id="datagrid" class="ag-theme-alpine" style="height: 600px; width:500px;" bind:this={gridContainer}></div>
+  
+  <style>
+	#datagrid {
+	  --ag-header-foreground-color: blue;
+	}
+	:global(.ag-header-cell) {
+	  background: orange;
+	  font-size: 16px;
+	}
+  </style>
+  
